84e5d75bad93a527877cf93ac00e25bae605b8aa
TheLeoLeger <leoallanleger@gmail.com>
2025-06-19 11:18:13 -0400

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,8 @@ def authenticate_drive():
     credentials_json = os.getenv("GOOGLE_CREDENTIALS")
     credentials_dict = json.loads(credentials_json)
     creds = service_account.Credentials.from_service_account_info(credentials_dict)
-    return build('drive', 'v3', credentials=creds)
+    service = build('drive', 'v3', credentials=creds)
+    return service
 # ENV VARS
 CREDENTIALS_JSON = os.environ.get("GOOGLE_CREDENTIALS")
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
